7159840bdc96fe716f6682b7aebe0d60b75bdb2c
Western-D <Westernd1812@gmail.com>
2022-12-15 14:56:40 +0200

    create Hometask7

--- src/hometask7/MyException.java
+++ src/hometask7/MyException.java
@@ -1,6 +1,6 @@
 package hometask7;
 
-public class MyException extends Exception{
+public abstract class MyException extends Exception{
     String message;
     public MyException(String exceptionMessage) {
         this.message = exceptionMessage;
